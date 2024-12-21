35ba558c9060382695a052a52e8341db87440cd4
David <123395249+dgreen42@users.noreply.github.com>
2024-12-21 12:36:53 -0500

    supress console windows

--- src/bin/vg-usb-interface_windows.rs
+++ src/bin/vg-usb-interface_windows.rs
@@ -1,7 +1,9 @@
 use vg_usb_interface::windows;
 use std::env;
+use windows_sys::Win32::System::Console::FreeConsole;
 
 fn main() {
     env::set_var("RUST_BACKTRACE", "1");
+    unsafe { FreeConsole() };
     windows();
 }
